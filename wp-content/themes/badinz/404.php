446be5674b530e47fd48cca7269e0cb49401c10f
Aleksandar Dojcinovic <aleksandar.dojcinovic@tranxactor.com>
2018-01-26 12:32:49 +0100

    404 page updates.

--- wp-content/themes/badinz/404.php
+++ wp-content/themes/badinz/404.php
@@ -8,53 +8,25 @@
  */
 
 get_header(); ?>
-
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
-
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'badinz' ); ?></h1>
-				</header><!-- .page-header -->
-
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'badinz' ); ?></p>
-
-					<?php
-						get_search_form();
-
-						the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'badinz' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'badinz' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-
-						the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main">
+            <section class="error-404 not-found">
+                <div class="container site-section">
+                    <div class="row">
+                        <div class="col-xs-12 text-center">
+													<header class="page-header">
+														<img class="img-responsive center-block" src="<?php echo get_template_directory_uri() ?>/images/404.png" alt="404">
+														<h1 class="page-title"><?php esc_html_e( 'Sorry, that page doesn&rsquo;t exists.', 'badinz' ); ?></h1>
+													</header><!-- .page-header -->
+													<div class="page-content">
+														<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php esc_html_e( 'Go back to home page', 'badinz' ); ?>"><?php esc_html_e( '&larr; Go back to home page', 'badinz' ); ?></a>
+													</div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </main>
+    </div>
 <?php
+
 get_footer();
